a9abfe766162637d4ec8c44a76e979cdac7c29b5
Simon Criado <simon_criado@simon-mpro13-34.local>
2022-09-16 16:26:36 +0200

    -

--- header.php
+++ header.php
@@ -1,25 +1,37 @@
+<!-- Here we start with a normal HTML template as this is the first thing that will appear in our page -->
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
+    <!-- Instead of entering all scripts (like fonts, icons, etc) directly here, we open PHP and use a function
+    that connects to the functions.php file -->
     <head>
-        <?php wp_head(); ?>
+      <meta charset="<?php bloginfo('charset'); ?>">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
-          <a href="#"><strong>Fictional</strong> University</a>
+          <!-- We open PHP to access the site url function. If no argument is passed it goes to the home page, otherwise it session_destroy
+          us to the desired page. For example '/about-us'   -->
+          <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
         </h1>
         <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
-              <li><a href="#">About Us</a></li>
+              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 12) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
             </ul>
+            <!-- <?php 
+            wp_nav_menu(array(
+              'theme_location' => 'headerMenuLocation'
+            ));
+            ?> -->
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
@@ -29,4 +41,7 @@
         </div>
       </div>
     </header>
+
+    <!-- IMPORTANT: we do not close the html and body tags here in the header. They will be closed in the footer file as
+    our page ends there... -->
     
--- page.php
+++ page.php
@@ -1,12 +1,71 @@
+<!-- File for our single pages. We also import here the header and footer -->
 <?php
 
 get_header();
 
 while(have_posts()) {
 the_post(); ?>
-<h1>This is a page, not a post</h1>
-<h2><?php the_title(); ?></h2>
-<?php the_content(); ?>
+
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>Dont forget to replace me later!</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+    
+    <!-- We enter php for the if statement, then we close it to write html and then we open it again to finish the 
+    if statement.
+    IMPORTANT: Instead of closing it like this it is better practice to use the "endif" or "endforeach" approach! -->
+    <?php 
+    // Get the parent post id based on the current page id. If there is no parent it will return 0 which is false
+    // and then the html wont be displayed. If there is a parent we will get its id (whatever number) which is true
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if ($theParent) { ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+      </div>
+    <?php }
+    ?>
+    
+      <?php 
+      $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+
+      if ($theParent or $testArray) { ?>  
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+        <ul class="min-list">
+            <?php 
+            if ($theParent){
+                $findChildrenOf = $theParent;
+            } else {
+                $findChildrenOf = get_the_ID();
+            }
+
+            wp_list_pages(array(
+                'title_li' => null,
+                'child_of' => $findChildrenOf,
+                'sort_column' => 'menu_order'
+            ));
+            ?>
+        </ul>
+      </div>
+      <?php } ?>
+
+
+      <div class="generic-content">
+      <?php the_content(); ?>  
+    </div>
+    </div>
+
 <?php }
 
 get_footer();
